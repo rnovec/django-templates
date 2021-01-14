27d447a2a0f834ea7506ccfb6000ef4bbd77e328
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-01-13 19:17:45 -0600

    Posts views and serializer

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    # viewset fields
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    # django filters fields
+    filterset_fields = '__all__'
+    search_fields = ['description', 'title']
+    ordering_fields = '__all__'
+    ordering = ['-created_at']
